e60ecfd5ee5c1893c35b802814fa23c8357a40c6
hingebase <zcliu@pku.edu.cn>
2025-10-05 14:43:19 +0800

    ⬆️ py-rattler 0.16.0

--- src/mahoraga/__init__.py
+++ src/mahoraga/__init__.py
@@ -18,31 +18,4 @@ __all__ = []
 
 import importlib.metadata
 
-import rattler.platform
-
 __version__ = importlib.metadata.version("mahoraga")
-
-if "emscripten-32" in rattler.platform.PlatformLiteral.__args__:
-    rattler.platform.PlatformLiteral.__args__ = (  # pyright: ignore[reportAttributeAccessIssue]
-        "noarch",
-        "linux-32",
-        "linux-64",
-        "linux-aarch64",
-        "linux-armv6l",
-        "linux-armv7l",
-        "linux-loong64",
-        "linux-ppc64le",
-        "linux-ppc64",
-        "linux-ppc",
-        "linux-s390x",
-        "linux-riscv32",
-        "linux-riscv64",
-        "osx-64",
-        "osx-arm64",
-        "win-32",
-        "win-64",
-        "win-arm64",
-        "emscripten-wasm32",
-        "wasi-wasm32",
-        "zos-z",
-    )
