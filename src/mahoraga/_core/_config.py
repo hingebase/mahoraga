582ae4db74a0c5906a3b063c05ab7995f29926f7
hingebase <zcliu@pku.edu.cn>
2026-05-01 05:39:27 +0800

    🔧 Avoid PyPI JSON API for some mirror sites

--- src/mahoraga/_core/_config.py
+++ src/mahoraga/_core/_config.py
@@ -253,16 +253,16 @@ class _PyPI(pydantic.BaseModel):
     html: list[_HttpUrl] = _adapter.validate_python([
         "https://mirror.nju.edu.cn/pypi/web/",
         "https://mirrors.aliyun.com/pypi/web/",
+        "https://mirrors.bfsu.edu.cn/pypi/web/",
         "https://mirrors.cloud.tencent.com/pypi/",
         "https://mirrors.huaweicloud.com/repository/pypi/",
         "https://mirrors.hust.edu.cn/pypi/web/",
         "https://mirrors.pku.edu.cn/pypi/web/",
         "https://mirrors.sustech.edu.cn/pypi/web/",
+        "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/",
     ])
     json_: Annotated[list[_HttpUrl], pydantic.Field(alias="json")] = (
         _adapter.validate_python([
-            "https://mirrors.bfsu.edu.cn/pypi/web/",
-            "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/",
             "https://mirrors.ustc.edu.cn/pypi/",
             "https://pypi.org/",
         ])
